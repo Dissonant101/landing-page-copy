f28ad92ebd91a911dfd5003e9c644c3f757e7e77
Dissonant101 <92066010+Dissonant101@users.noreply.github.com>
2024-10-27 19:25:04 -0400

    feat: add body elements and styling to text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,52 +13,71 @@ const Home = () => {
     <Container className="overflow-x-hidden bg-[#faf4ee]">
       <Navbar />
       <Section>
-        <h1>Company ownership, from the future</h1>
-        <p>A next generation platform for modern founders, powered by AI.</p>
-        <Button>REQUEST A DEMO</Button>
-        <video autoPlay className="my-8 w-full max-w-[900px]" loop muted>
-          <source
-            src="https://storage.googleapis.com/mantle-website-assets/videos/SpinningCircles.mp4"
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
+        <Box className="mx-[110px] flex max-w-[720px] flex-col place-items-center py-6 text-center">
+          <h1 className="text-7xl/[72px]">
+            Company ownership, from the future
+          </h1>
+          <p className="my-6 text-lg font-medium">
+            A next generation platform for modern founders, powered by AI.
+          </p>
+          <Button className="flex-none">REQUEST A DEMO</Button>
+          <video
+            autoPlay
+            className="w-full max-w-[900px] pt-4 outline-none"
+            loop
+            muted
+          >
+            <source
+              src="https://storage.googleapis.com/mantle-website-assets/videos/SpinningCircles.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </Box>
       </Section>
       <Section>
-        <h1>Manage like a pro</h1>
-        <p>
-          Take control of your ownership with a next-generation platform that
-          lets you track, issue, and manage ownership. Be your own expert and
-          scale back on your professional fees.
-        </p>
+        <Box className="flex max-w-[528px] flex-col place-items-center text-center">
+          <h1 className="text-5xl/[60px] font-medium">Manage like a pro</h1>
+          <p className="my-6 text-lg">
+            Take control of your ownership with a next-generation platform that
+            lets you track, issue, and manage ownership. Be your own expert and
+            scale back on your professional fees.
+          </p>
+        </Box>
         <Card>
-          <Box className="flex flex-col place-items-center px-4 py-16">
+          <Box className="flex max-w-[528px] flex-col place-items-center px-4 py-16">
             <Box>
               <Image
                 alt="logo"
+                className="h-32 w-32"
                 height={343}
                 src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e05_temp_AI.png"
                 width={343}
               />
             </Box>
             <Box>PLACEHOLDER</Box>
-            <h2>Get it done with your AI Co-pilot</h2>
-            <p>
+            <h2 className="my-6 text-xl/[32px] font-medium">
+              Get it done with your AI Co-pilot
+            </h2>
+            <p className="text-lg">
               Breeze through essential tasks, pull critical information, and
               visualize your ownership data instantly.
             </p>
           </Box>
         </Card>
         <Card>
-          <Box className="flex w-1/2 flex-col place-items-center px-4 py-16">
+          <Box className="flex w-1/2 max-w-[528px] flex-col place-items-center px-12 py-16">
             <Image
               alt="logo"
+              className="h-9 w-9"
               height={343}
               src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e07_img_icon_workflow.svg"
               width={343}
             />
-            <h2>Supercharge your workflow</h2>
-            <p>
+            <h2 className="text-xl/[32px] font-medium">
+              Supercharge your workflow
+            </h2>
+            <p className="my-6 text-lg">
               Transform tasks such as issuing equity and stakeholder
               communications into remarkably simple, automated workflows. Get
               back to focusing on building your business in minutes, not hours.
@@ -84,15 +103,18 @@ const Home = () => {
               width={343}
             />
           </Box>
-          <Box className="flex w-1/2 flex-col place-items-center px-4 py-16">
+          <Box className="flex w-1/2 max-w-[528px] flex-col place-items-center px-12 py-16">
             <Image
               alt="logo"
+              className="h-9 w-9"
               height={343}
               src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e06_img_icon_decisions.svg"
               width={343}
             />
-            <h2>Make decisions, not mistakes</h2>
-            <p>
+            <h2 className="text-xl/[32px] font-medium">
+              Make decisions, not mistakes
+            </h2>
+            <p className="my-6 text-lg">
               Handle everything from modeling hiring plans to assessing
               jargon-heavy term sheets without touching a spreadsheet.
               In-context advice is there when you need it, giving you clear
@@ -102,34 +124,40 @@ const Home = () => {
         </Card>
       </Section>
       <Section>
-        <h1>Up and to the right</h1>
-        <p>
-          Support at every stage, from your garage to a thriving public company.
-          Seamless assistance throughout your journey.
-        </p>
+        <Box className="max-w-[400px]">
+          <h1 className="text-5xl/[60px] font-medium">Up and to the right</h1>
+          <p className="my-6 text-lg">
+            Support at every stage, from your garage to a thriving public
+            company. Seamless assistance throughout your journey.
+          </p>
+        </Box>
       </Section>
-      <Box className="flex place-content-center bg-white">
+      <Box className="my-16 flex place-content-center bg-white py-16">
         <Box className="flex max-w-[1400px] place-content-center gap-8">
           <Image
             alt="logo"
+            className="h-[140px] w-[187px]"
             height={600}
             src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/654beb40a4199812b5b99f45_fortune-black.png"
             width={800}
           />
           <Image
             alt="logo"
+            className="h-[140px] w-[187px]"
             height={600}
             src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/654beb4019075ca39d2d7568_techcrunch-black.png"
             width={800}
           />
           <Image
             alt="logo"
+            className="h-[140px] w-[187px]"
             height={600}
             src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/654beb95a02e4305dab55568_axios-black.png"
             width={800}
           />
           <Image
             alt="logo"
+            className="h-[140px] w-[187px]"
             height={600}
             src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/663bde918911cddfb88c51cc_betakit-black-logo.png"
             width={800}
@@ -137,14 +165,88 @@ const Home = () => {
         </Box>
       </Box>
       <Section>
-        <h1>
-          Manage everything in one place with simple, straightforward tools
-          augmented with AI
-        </h1>
+        <Box className="max-w-[940px]">
+          <h1 className="text-6xl/[72px] font-medium">
+            Manage everything in one place with simple, straightforward tools
+            augmented with AI
+          </h1>
+          <Separator className="my-16" />
+          <Box className="flex flex-col gap-16">
+            <Box className="flex justify-between gap-8">
+              <Box className="w-1/3">
+                <h2 className="text-lg/[30px] font-medium">Fundraising</h2>
+                <p className="text-md my-4">
+                  Model rounds, issue SAFEs, and track the whole process all in
+                  one place.
+                </p>
+              </Box>
+              <Box className="w-1/3">
+                <h2 className="text-lg/[30px] font-medium">Cap Tables</h2>
+                <p className="text-md my-4">
+                  Effortlessly track and organize your cap table, providing a
+                  transparent view of your company&apos;s ownership.
+                </p>
+              </Box>
+              <Box className="w-1/3">
+                <h2 className="text-lg/[30px] font-medium">
+                  Planning and Forecasting
+                </h2>
+                <p className="text-md my-4">
+                  Powerful tools to model your hiring needs with visualizations
+                  that streamline your decision making.
+                </p>
+              </Box>
+            </Box>
+            <Box className="flex justify-between gap-8">
+              <Box className="w-1/3">
+                <h2 className="text-lg/[30px] font-medium">Issue Equity</h2>
+                <p className="text-md my-4">
+                  Create and manage multiple option plans and issue option
+                  grants with predictive workflows.
+                </p>
+              </Box>
+              <Box className="w-1/3">
+                <h2 className="text-lg/[30px] font-medium">
+                  Automated Signing
+                </h2>
+                <p className="text-md my-4">
+                  Setup approvals and templates in a single click. Automatic
+                  reminders for outstanding actions to keep everything moving.
+                </p>
+              </Box>
+              <Box className="w-1/3">
+                <h2 className="text-lg/[30px] font-medium">Data Rooms</h2>
+                <p className="text-md my-4">
+                  No more scattered documents in inboxes and siloed information
+                  systems. All your documents in one place, found instantly when
+                  you need them.
+                </p>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
       </Section>
       <Section className="mb-24">
-        <Button>SEE A DEMO</Button>
-        <Button>GET STARTED</Button>
+        <Card color="#0e0f11">
+          <Box className="flex flex-col place-items-center px-32 py-16">
+            <h1 className="text-5xl/[60px] font-medium text-white">
+              Get started for free
+            </h1>
+            <p className="my-10 text-lg text-white">
+              Level up your equity management for $0 —no credit card required.
+              Flat-rate paid plans start at $100 per month for unlimited
+              stakeholders.
+            </p>
+            <Box>
+              <Button className="outline outline-1 outline-white">
+                SEE A DEMO
+              </Button>
+              <Button className="bg-white" variant="secondary">
+                GET STARTED
+              </Button>
+            </Box>
+          </Box>
+        </Card>
       </Section>
       <Separator />
       <Footer />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,30 +18,30 @@ const Footer = () => {
           </Box>
           <Box className="flex gap-16">
             <Box className="flex flex-col gap-2">
-              <h2>Products</h2>
-              <p>Cap Table Management</p>
-              <p>Planning & Forecasting</p>
-              <p>LP Automation</p>
+              <h2 className="text-lg/[30px] font-medium">Products</h2>
+              <p className="text-md">Cap Table Management</p>
+              <p className="text-md">Planning & Forecasting</p>
+              <p className="text-md">LP Automation</p>
             </Box>
             <Box className="flex flex-col gap-2">
-              <h2>Solutions</h2>
-              <p>For Founders</p>
-              <p>For Limited Partners</p>
-              <p>For Law Firms</p>
+              <h2 className="text-lg/[30px] font-medium">Solutions</h2>
+              <p className="text-md">For Founders</p>
+              <p className="text-md">For Limited Partners</p>
+              <p className="text-md">For Law Firms</p>
             </Box>
             <Box className="flex flex-col gap-2">
-              <h2>Resources</h2>
-              <p>Migrate from Carta</p>
-              <p>Migrate from Pulley</p>
-              <p>Migrate from Shareworks</p>
-              <p>409A Valuations</p>
-              <p>Privacy Policy</p>
-              <p>Blog</p>
+              <h2 className="text-lg/[30px] font-medium">Resources</h2>
+              <p className="text-md">Migrate from Carta</p>
+              <p className="text-md">Migrate from Pulley</p>
+              <p className="text-md">Migrate from Shareworks</p>
+              <p className="text-md">409A Valuations</p>
+              <p className="text-md">Privacy Policy</p>
+              <p className="text-md">Blog</p>
             </Box>
             <Box className="flex flex-col gap-2">
-              <h2>Company</h2>
-              <p>Our Team</p>
-              <p>Security</p>
+              <h2 className="text-lg/[30px] font-medium">Company</h2>
+              <p className="text-md">Our Team</p>
+              <p className="text-md">Security</p>
               <Box className="flex items-center gap-4">
                 <Image
                   alt="logo"
@@ -67,9 +67,8 @@ const Footer = () => {
         </Box>
         <Separator className="min-w-[500px] max-w-[1400px]" />
         <Box className="flex w-full min-w-[500px] max-w-[1400px] items-center justify-between py-16">
-          <p>
-            © 2024 Ten Key Labs Incorporated DBA Mantle ·{' '}
-            <a href="">Terms of Use</a>
+          <p className="text-md font-medium text-[#76777a]">
+            © Remade by Steven Chen · <a href="">Terms of Use</a>
           </p>
           <Box className="flex items-center gap-16">
             <Image
@@ -88,90 +87,6 @@ const Footer = () => {
         </Box>
       </Box>
     </Container>
-    // <Container className="flex h-full w-full place-content-center">
-    //   <Box className="flex w-full justify-between py-8">
-    //     <Box>
-    //       <Image
-    //         alt="logo"
-    //         className="min-h-[36px] min-w-[163px]"
-    //         height={36}
-    //         src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/655bac9fc699356b43d171a0_Mantle_Logo_Lockup.svg"
-    //         width={163}
-    //       />
-    //     </Box>
-    //     <Box className="flex gap-16">
-    //       <Box className="flex flex-col gap-2">
-    //         <h1>Products</h1>
-    //         <p>Cap Table Management</p>
-    //         <p>Planning & Forecasting</p>
-    //         <p>LP Automation</p>
-    //       </Box>
-    //       <Box className="flex flex-col gap-2">
-    //         <h1>Solutions</h1>
-    //         <p>For Founders</p>
-    //         <p>For Limited Partners</p>
-    //         <p>For Law Firms</p>
-    //       </Box>
-    //       <Box className="flex flex-col gap-2">
-    //         <h1>Resources</h1>
-    //         <p>Migrate from Carta</p>
-    //         <p>Migrate from Pulley</p>
-    //         <p>Migrate from Shareworks</p>
-    //         <p>409A Valuations</p>
-    //         <p>Privacy Policy</p>
-    //         <p>Blog</p>
-    //       </Box>
-    //       <Box className="flex flex-col gap-2">
-    //         <h1>Company</h1>
-    //         <p>Our Team</p>
-    //         <p>Security</p>
-    //         <Box className="flex items-center gap-4">
-    //           <Image
-    //             alt="logo"
-    //             height={20}
-    //             src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/663bd9cb48e225d03ac625a5_logo-AICPA-SOC.png"
-    //             width={20}
-    //           />
-    //           <Image
-    //             alt="logo"
-    //             height={20}
-    //             src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/663bd9cb48e225d03ac625a5_logo-AICPA-SOC.png"
-    //             width={20}
-    //           />
-    //           <Image
-    //             alt="logo"
-    //             height={20}
-    //             src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/663bd9cb48e225d03ac625a5_logo-AICPA-SOC.png"
-    //             width={20}
-    //           />
-    //         </Box>
-    //       </Box>
-    //     </Box>
-    //   </Box>
-    //   <Separator />
-    //   <Box className="flex justify-between py-16">
-    //     <p>
-    //       © 2024 Ten Key Labs Incorporated DBA Mantle ·{' '}
-    //       <a href="">Terms of Use</a>
-    //     </p>
-    //     <Box className="flex items-center gap-4">
-    //       <Image
-    //         alt="logo"
-    //         className="min-h-[42px] min-w-[150px]"
-    //         height={42}
-    //         src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/663bd9ba8553e48cc93eade1_logo-google-cloud.png"
-    //         width={150}
-    //       />
-    //       <Image
-    //         alt="logo"
-    //         className="min-h-[58px] min-w-[58px]"
-    //         height={58}
-    //         src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/663bd9cb48e225d03ac625a5_logo-AICPA-SOC.png"
-    //         width={58}
-    //       />
-    //     </Box>
-    //   </Box>
-    // </Container>
   );
 };
 
