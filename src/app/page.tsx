000e3d8a6cc4ef58697f92eca30346063d9eb6c4
Dissonant101 <92066010+Dissonant101@users.noreply.github.com>
2024-10-27 22:20:55 -0400

    feat: add fade up effect on scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Animate from '@/components/Animate';
 import Card from '@/components/Card';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
@@ -19,126 +20,145 @@ const Home = () => {
       <Box className="h-24"></Box>
       <Section>
         <Box className="mx-[110px] flex max-w-[720px] flex-col place-items-center py-6 text-center">
-          <h1 className="text-7xl/[72px]">
-            Company ownership, from the future
-          </h1>
-          <p className="my-6 text-lg font-medium">
-            A next generation platform for modern founders, powered by AI.
-          </p>
-          <Button className="flex-none">REQUEST A DEMO</Button>
-          <video
-            autoPlay
-            className="w-full max-w-[900px] pt-4 outline-none"
-            loop
-            muted
-          >
-            <source
-              src="https://storage.googleapis.com/mantle-website-assets/videos/SpinningCircles.mp4"
-              type="video/mp4"
-            />
-            Your browser does not support the video tag.
-          </video>
-        </Box>
-      </Section>
-      <Section>
-        <Box className="flex max-w-[528px] flex-col place-items-center text-center">
-          <h1 className="text-5xl/[60px] font-medium">Manage like a pro</h1>
-          <p className="my-6 text-lg">
-            Take control of your ownership with a next-generation platform that
-            lets you track, issue, and manage ownership. Be your own expert and
-            scale back on your professional fees.
-          </p>
-        </Box>
-        <Card className="bg-[#f7efe8]">
-          <Box className="flex max-w-[528px] flex-col place-items-center px-4 py-16">
-            <Box>
-              <Image
-                alt="logo"
-                className="h-32 w-32"
-                height={343}
-                src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e05_temp_AI.png"
-                width={343}
+          <Animate>
+            <h1 className="text-7xl/[72px]">
+              Company ownership, from the future
+            </h1>
+            <p className="my-6 text-lg font-medium">
+              A next generation platform for modern founders, powered by AI.
+            </p>
+            <Button className="flex-none">REQUEST A DEMO</Button>
+          </Animate>
+          <Animate>
+            <video
+              autoPlay
+              className="w-full max-w-[900px] pt-4 outline-none"
+              loop
+              muted
+            >
+              <source
+                src="https://storage.googleapis.com/mantle-website-assets/videos/SpinningCircles.mp4"
+                type="video/mp4"
               />
-            </Box>
-            <Lottie
-              className="w-full"
-              path="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e0c_typewritertext.json"
-              play
-            />
-            <h2 className="my-6 text-xl/[32px] font-medium">
-              Get it done with your AI Co-pilot
-            </h2>
-            <p className="text-lg">
-              Breeze through essential tasks, pull critical information, and
-              visualize your ownership data instantly.
-            </p>
-          </Box>
-        </Card>
-        <Card className="bg-[#f7efe8]">
-          <Box className="flex w-1/2 max-w-[528px] flex-col place-items-center px-12 py-16">
-            <Image
-              alt="logo"
-              className="h-9 w-9"
-              height={343}
-              src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e07_img_icon_workflow.svg"
-              width={343}
-            />
-            <h2 className="text-xl/[32px] font-medium">
-              Supercharge your workflow
-            </h2>
-            <p className="my-6 text-lg">
-              Transform tasks such as issuing equity and stakeholder
-              communications into remarkably simple, automated workflows. Get
-              back to focusing on building your business in minutes, not hours.
-            </p>
-          </Box>
-          <Box className="w-1/2">
-            <Lottie
-              path="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e0b_ANIM_asset_workflow.json"
-              play
-            />
-          </Box>
-        </Card>
-        <Card className="bg-[#f7efe8]">
-          <Box className="w-1/2">
-            <Lottie
-              path="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e0a_ANIM_asset_decision.json"
-              play
-            />
-          </Box>
-          <Box className="flex w-1/2 max-w-[528px] flex-col place-items-center px-12 py-16">
-            <Image
-              alt="logo"
-              className="h-9 w-9"
-              height={343}
-              src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e06_img_icon_decisions.svg"
-              width={343}
-            />
-            <h2 className="text-xl/[32px] font-medium">
-              Make decisions, not mistakes
-            </h2>
-            <p className="my-6 text-lg">
-              Handle everything from modeling hiring plans to assessing
-              jargon-heavy term sheets without touching a spreadsheet.
-              In-context advice is there when you need it, giving you clear
-              insight to make decisions.
-            </p>
-          </Box>
-        </Card>
+              Your browser does not support the video tag.
+            </video>
+          </Animate>
+        </Box>
       </Section>
       <Section>
-        <Box className="max-w-[400px]">
-          <h1 className="text-5xl/[60px] font-medium">Up and to the right</h1>
-          <p className="my-6 text-lg">
-            Support at every stage, from your garage to a thriving public
-            company. Seamless assistance throughout your journey.
-          </p>
-        </Box>
-        <Lottie
-          path="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/65fde568b3d496beae3da68a_timeline_animation_update.json"
-          play
-          speed={0.5}
-        />
+        <Animate>
+          <Box className="flex max-w-[528px] flex-col place-items-center text-center">
+            <h1 className="text-5xl/[60px] font-medium">Manage like a pro</h1>
+            <p className="my-6 text-lg">
+              Take control of your ownership with a next-generation platform
+              that lets you track, issue, and manage ownership. Be your own
+              expert and scale back on your professional fees.
+            </p>
+          </Box>
+        </Animate>
+      </Section>
+      <Section>
+        <Container maxWidth="528px">
+          <Animate>
+            <Card className="bg-[#f7efe8]">
+              <Box className="flex flex-col place-items-center px-4 py-16">
+                <Box>
+                  <Image
+                    alt="logo"
+                    className="h-32 w-32"
+                    height={343}
+                    src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e05_temp_AI.png"
+                    width={343}
+                  />
+                </Box>
+                <Lottie
+                  className="w-full"
+                  path="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e0c_typewritertext.json"
+                  play
+                />
+                <h2 className="my-6 text-xl/[32px] font-medium">
+                  Get it done with your AI Co-pilot
+                </h2>
+                <p className="text-lg">
+                  Breeze through essential tasks, pull critical information, and
+                  visualize your ownership data instantly.
+                </p>
+              </Box>
+            </Card>
+          </Animate>
+          <Animate>
+            <Card className="bg-[#f7efe8]">
+              <Box className="flex w-1/2 flex-col place-items-center px-12 py-16">
+                <Image
+                  alt="logo"
+                  className="h-9 w-9"
+                  height={343}
+                  src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e07_img_icon_workflow.svg"
+                  width={343}
+                />
+                <h2 className="text-xl/[32px] font-medium">
+                  Supercharge your workflow
+                </h2>
+                <p className="my-6 text-lg">
+                  Transform tasks such as issuing equity and stakeholder
+                  communications into remarkably simple, automated workflows.
+                  Get back to focusing on building your business in minutes, not
+                  hours.
+                </p>
+              </Box>
+              <Box className="w-1/2">
+                <Lottie
+                  path="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e0b_ANIM_asset_workflow.json"
+                  play
+                />
+              </Box>
+            </Card>
+          </Animate>
+          <Animate>
+            <Card className="bg-[#f7efe8]">
+              <Box className="w-1/2">
+                <Lottie
+                  path="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e0a_ANIM_asset_decision.json"
+                  play
+                />
+              </Box>
+              <Box className="flex w-1/2 flex-col place-items-center px-12 py-16">
+                <Image
+                  alt="logo"
+                  className="h-9 w-9"
+                  height={343}
+                  src="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/64b05c5307d994750a3d2e06_img_icon_decisions.svg"
+                  width={343}
+                />
+                <h2 className="text-xl/[32px] font-medium">
+                  Make decisions, not mistakes
+                </h2>
+                <p className="my-6 text-lg">
+                  Handle everything from modeling hiring plans to assessing
+                  jargon-heavy term sheets without touching a spreadsheet.
+                  In-context advice is there when you need it, giving you clear
+                  insight to make decisions.
+                </p>
+              </Box>
+            </Card>
+          </Animate>
+        </Container>
+      </Section>
+      <Section>
+        <Animate>
+          <Box className="max-w-[400px]">
+            <h1 className="text-5xl/[60px] font-medium">Up and to the right</h1>
+            <p className="my-6 text-lg">
+              Support at every stage, from your garage to a thriving public
+              company. Seamless assistance throughout your journey.
+            </p>
+          </Box>
+          <Lottie
+            path="https://cdn.prod.website-files.com/64b05c5307d994750a3d2dde/65fde568b3d496beae3da68a_timeline_animation_update.json"
+            play
+            speed={0.5}
+          />
+        </Animate>
       </Section>
       <Box className="my-16 flex place-content-center bg-white py-16">
         <Box className="flex max-w-[1400px] place-content-center gap-8">
@@ -174,61 +194,75 @@ const Home = () => {
       </Box>
       <Section>
         <Box className="max-w-[940px]">
-          <h1 className="text-6xl/[72px] font-medium">
-            Manage everything in one place with simple, straightforward tools
-            augmented with AI
-          </h1>
+          <Animate>
+            <h1 className="text-6xl/[72px] font-medium">
+              Manage everything in one place with simple, straightforward tools
+              augmented with AI
+            </h1>
+          </Animate>
           <Separator className="my-16" />
           <Box className="flex flex-col gap-16">
             <Box className="flex justify-between gap-8">
               <Box className="w-1/3">
-                <h2 className="text-lg/[30px] font-medium">Fundraising</h2>
-                <p className="text-md my-4">
-                  Model rounds, issue SAFEs, and track the whole process all in
-                  one place.
-                </p>
+                <Animate>
+                  <h2 className="text-lg/[30px] font-medium">Fundraising</h2>
+                  <p className="text-md my-4">
+                    Model rounds, issue SAFEs, and track the whole process all
+                    in one place.
+                  </p>
+                </Animate>
               </Box>
               <Box className="w-1/3">
-                <h2 className="text-lg/[30px] font-medium">Cap Tables</h2>
-                <p className="text-md my-4">
-                  Effortlessly track and organize your cap table, providing a
-                  transparent view of your company&apos;s ownership.
-                </p>
+                <Animate>
+                  <h2 className="text-lg/[30px] font-medium">Cap Tables</h2>
+                  <p className="text-md my-4">
+                    Effortlessly track and organize your cap table, providing a
+                    transparent view of your company&apos;s ownership.
+                  </p>
+                </Animate>
               </Box>
               <Box className="w-1/3">
-                <h2 className="text-lg/[30px] font-medium">
-                  Planning and Forecasting
-                </h2>
-                <p className="text-md my-4">
-                  Powerful tools to model your hiring needs with visualizations
-                  that streamline your decision making.
-                </p>
+                <Animate>
+                  <h2 className="text-lg/[30px] font-medium">
+                    Planning and Forecasting
+                  </h2>
+                  <p className="text-md my-4">
+                    Powerful tools to model your hiring needs with
+                    visualizations that streamline your decision making.
+                  </p>
+                </Animate>
               </Box>
             </Box>
             <Box className="flex justify-between gap-8">
               <Box className="w-1/3">
-                <h2 className="text-lg/[30px] font-medium">Issue Equity</h2>
-                <p className="text-md my-4">
-                  Create and manage multiple option plans and issue option
-                  grants with predictive workflows.
-                </p>
+                <Animate>
+                  <h2 className="text-lg/[30px] font-medium">Issue Equity</h2>
+                  <p className="text-md my-4">
+                    Create and manage multiple option plans and issue option
+                    grants with predictive workflows.
+                  </p>
+                </Animate>
               </Box>
               <Box className="w-1/3">
-                <h2 className="text-lg/[30px] font-medium">
-                  Automated Signing
-                </h2>
-                <p className="text-md my-4">
-                  Setup approvals and templates in a single click. Automatic
-                  reminders for outstanding actions to keep everything moving.
-                </p>
+                <Animate>
+                  <h2 className="text-lg/[30px] font-medium">
+                    Automated Signing
+                  </h2>
+                  <p className="text-md my-4">
+                    Setup approvals and templates in a single click. Automatic
+                    reminders for outstanding actions to keep everything moving.
+                  </p>
+                </Animate>
               </Box>
               <Box className="w-1/3">
-                <h2 className="text-lg/[30px] font-medium">Data Rooms</h2>
-                <p className="text-md my-4">
-                  No more scattered documents in inboxes and siloed information
-                  systems. All your documents in one place, found instantly when
-                  you need them.
-                </p>
+                <Animate>
+                  <h2 className="text-lg/[30px] font-medium">Data Rooms</h2>
+                  <p className="text-md my-4">
+                    No more scattered documents in inboxes and siloed
+                    information systems. All your documents in one place, found
+                    instantly when you need them.
+                  </p>
+                </Animate>
               </Box>
             </Box>
           </Box>
---
+++ src/components/Animate.tsx
@@ -0,0 +1,23 @@
+import AOS from 'aos';
+import { PropsWithChildren } from 'react';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
+interface AnimateProps extends PropsWithChildren {
+  className?: string;
+  effect?: string;
+}
+
+const Animate = ({ children, className, effect = 'fade-up' }: AnimateProps) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <div className={className} data-aos={effect} data-aos-once="true">
+      {children}
+    </div>
+  );
+};
+
+export default Animate;
